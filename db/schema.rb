345a904dbfb39d54d27822638129049ac0cba62e
twingo-b <twingo.b@gmail.com>
2016-06-07 16:32:10 +0900

    scaffoldで生成されたコードを自分で書いてみる

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607020443) do
+ActiveRecord::Schema.define(version: 20160607061431) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "department", limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
