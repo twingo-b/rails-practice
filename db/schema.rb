13df731a70c451eeda972c695f88798f1ae7ff1d
twingo-b <twingo.b@gmail.com>
2016-06-08 14:49:37 +0900

    my answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608011454) do
+ActiveRecord::Schema.define(version: 20160608035256) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "body",       limit: 255
@@ -28,7 +34,10 @@ ActiveRecord::Schema.define(version: 20160608011454) do
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "blog_id",    limit: 4
   end
+
+  add_index "entries", ["blog_id"], name: "index_entries_on_blog_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 20160608011454) do
   end
 
   add_foreign_key "comments", "entries"
+  add_foreign_key "entries", "blogs"
 end
