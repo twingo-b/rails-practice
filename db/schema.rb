73471a816b12d8e1cc345e06d1c8ad4e0ca86cd4
twingo-b <twingo.b@gmail.com>
2016-06-14 19:42:33 +0900

    comments.bodyの型をtextに修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608035256) do
+ActiveRecord::Schema.define(version: 20160614103129) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 20160608035256) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "body",       limit: 255
+    t.text     "body",       limit: 65535
     t.string   "status",     limit: 255
     t.integer  "entry_id",   limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "comments", ["entry_id"], name: "index_comments_on_entry_id", using: :btree
